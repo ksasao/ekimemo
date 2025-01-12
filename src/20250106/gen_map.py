8a75610d50bc5e85930fb4a215e82e65116122f2
ksasao <k.sasao@gmail.com>
2025-01-12 17:40:22 +0900

    吹き出しにふりがな追加。赤マーカーのカウント数表示を追加

--- src/20250106/gen_map.py
+++ src/20250106/gen_map.py
@@ -1,3 +1,4 @@
+import re
 import json
 import requests
 
@@ -13,15 +14,19 @@ stations = response.json()
 marker_data = [{
     'lat': station['lat'],
     'lon': station['lng'],
-    'name': station['name']
+    'name': station['name'],
+    'name_kana': station['name_kana']
 } for station in stations if any(char in station['name'] for char in filter_str)]
 
 # HTMLテンプレートを読み込む
 with open('template.html', 'r', encoding='utf-8') as file:
     html_template = file.read() # markerDataの部分を生成されたデータで置き換え 
     marker_data_json = json.dumps(marker_data, ensure_ascii=False) 
-    html_content = html_template.replace("REPLACE_MARKER_DATA",marker_data_json)
-
+    html_content = re.sub(
+         r'var markerData = \[.*?\]; // REPLACE_MARKER_DATA',
+         f'var markerData = {marker_data_json};'
+         , html_template
+    )
 # HTMLファイルに書き込む
 with open('../../docs/20250106/index.html', 'w', encoding='utf-8') as file:
     file.write(html_content)
