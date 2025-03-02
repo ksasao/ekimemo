764a0d8668eb881ce5122d532b4d44338b59093e
ksasao <k.sasao@gmail.com>
2025-03-02 12:44:42 +0900

    iOS用デスクトップアイコンを追加

--- src/20250304/gen_map.py
+++ src/20250304/gen_map.py
@@ -2,6 +2,8 @@ import re
 import json
 import requests
 import pandas as pd
+import shutil
+import os
 
 # CSVファイルを読み込む
 csv_file = 'data.csv'
@@ -18,13 +20,21 @@ print(marker_data)
 
 # HTMLテンプレートを読み込む
 with open('template.html', 'r', encoding='utf-8') as file:
-    html_template = file.read() # markerDataの部分を生成されたデータで置き換え 
-    marker_data_json = json.dumps(marker_data, ensure_ascii=False) 
-    html_content = re.sub(
-         r'var markerData = \[.*?\]; // REPLACE_MARKER_DATA',
-         f'var markerData = {marker_data_json};'
-         , html_template
-    )
+    html_template = file.read()
+
+# markerDataの部分を生成されたデータで置き換え 
+marker_data_json = json.dumps(marker_data, ensure_ascii=False) 
+html_content = re.sub(
+     r'var markerData = \[.*?\]; // REPLACE_MARKER_DATA',
+     f'var markerData = {marker_data_json};',
+     html_template
+)
+
 # HTMLファイルに書き込む
-with open('../../docs/20250304/index.html', 'w', encoding='utf-8') as file:
+output_path = "../../docs/20250304/"
+os.makedirs(output_path, exist_ok=True)
+with open(os.path.join(output_path, 'index.html'), 'w', encoding='utf-8') as file:
     file.write(html_content)
+
+# icon.pngをoutput_pathにコピーする
+shutil.copy('icon.png',output_path)
